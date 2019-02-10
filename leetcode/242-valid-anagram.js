8fae614cbe20083062f70c8ba7a14027c44ee946
Jinyan <jinyan.shen21@gmail.com>
2019-02-10 12:51:54 -0500

    leetcode 242 passed

---
+++ leetcode/242-valid-anagram.js
@@ -0,0 +1,22 @@
+// Given two strings s and t , write a function to determine if t is an anagram of s.
+
+// Example 1:
+
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+
+// Input: s = "rat", t = "car"
+// Output: false
+// Note:
+// You may assume the string contains only lowercase alphabets.
+
+// Follow up:
+// What if the inputs contain unicode characters? How would you adapt your solution to such case?
+
+const isAnagram = function(s, t) {big O(m + n)
+    let s1 = s.split('').sort().join('');
+    let t1 = t.split('').sort().join('');
+    return s1 === t1
+    
+};
